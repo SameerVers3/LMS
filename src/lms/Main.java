b3582cd3f28c0dc4beb2b6585b4669980b4e1f78
Sameer <111224603+Sameer-Verse@users.noreply.github.com>
2023-05-08 04:38:40 +0500

    .

--- src/lms/Main.java
+++ src/lms/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("---");
+        System.out.println("Ab thora kaam please");
     }
 
 }
