5916bfda8361d319962812a7a3d8db9b8d3f7bde
amna247 <amnamansoor2468@gmail.com>
2023-05-08 04:40:15 +0500

    First commit

--- src/lms/Main.java
+++ src/lms/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("---");
         System.out.println("Ab thora kaam please");
+        System.out.println("Yes yes");
     }
 
 }
